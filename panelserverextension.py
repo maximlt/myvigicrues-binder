c64e258661f4d1e6d64365d944ce6643d409c65d
maximelqt <maximeliquet@free.fr>
2019-11-03 15:17:25 +0100

    fix script

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
-def load_jupyter_server_extension(nbapp)
-    serve the clifford.ipynb directory with bokeh server
-    Popen([panel, serve, clifford.ipynb, --allow-websocket-origin=])
+def load_jupyter_server_extension(nbapp):
+    """serve the clifford.ipynb directory with bokeh server"""
+    Popen(["panel", "serve", "myvigicrues_binder.ipynb", "--allow-websocket-origin=*"])
