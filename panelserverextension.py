717ff2ea9ec1b6884c98613abf08bb7de6d586fd
maximelqt <maximeliquet@free.fr>
2019-11-03 22:19:05 +0100

    new env

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the notebook with bokeh server"""
-    Popen(["panel", "serve", "dummy.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "myvigicrues_binder.ipynb", "--allow-websocket-origin=*"])
